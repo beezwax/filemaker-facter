b3c5aaeda59c4a9cb57c9958e227eff1cb1ec0fe
Simon Brown <simon_b@beezwax.net>
2015-02-08 10:59:35 -0800

    added constants

--- filemaker-facts/filemaker_utils.rb
+++ filemaker-facts/filemaker_utils.rb
@@ -2,8 +2,31 @@
 #
 # HISTORY
 #   2015-02-06 simon_b: created file
+#   2015-02-08 simon_b: added constants
+
+# Paths to the fmsadmin command.
+FMSADMIN_MAC = "/usr/bin/fmsadmin"
+FMSADMIN_WIN = ""C:/Program Files/FileMaker/FileMaker Server/fmsadmin"
+
+# LOG FILE PATHS
+LOG_EVENTS_MAC = "/Library/FileMaker Server/Logs/Eventss.log"
+LOG_STATS_MAC = "/Library/FileMaker Server/Logs/Stats.log"
+
+# STATS LOG COLUMNS
+# Values for tab-delimited columns in the Stats.log file.
+STATS_NETIN = 1
+STATS_NETOUT = 2
+STATS_DISKREAD = 3
+STATS_DISKWRITE = 4
+STATS_OPENDBS = 7
+
 
 ## filemaker_utils.rb
+
+def log_is_current (path)
+  stats_updated_at = File.ctime(stats_path)
+  return (Time.new() - stats_updated_at) < (5*60)
+end
 
 # From http://stackoverflow.com/questions/754494/reading-the-last-n-lines-of-a-file-in-ruby/28221975
 
@@ -38,5 +61,5 @@ def tail(path, n)
   end
 
   file.seek(offset)
-  data = file.read
+  return file.read
 end

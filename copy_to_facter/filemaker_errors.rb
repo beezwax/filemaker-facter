e28d9772c155f20415d9859fbe333ff17907bc7b
Simon Brown <simon_b@beezwax.net>
2016-01-05 16:53:23 -0800

    time range filtering completed

--- copy_to_facter/filemaker_errors.rb
+++ copy_to_facter/filemaker_errors.rb
@@ -5,49 +5,56 @@
 # NOTES:
 #   Assumes logging interval is set to the default 30 second interval and the Event.log has not been rolled recently.
 #   Testing looks like this:
-#     export FACTERLIB="/Users/simon_b/filemaker-facter/filemaker-facts"; facter -d filemaker_file_count
+#     export FACTERLIB="/Users/simon_b/filemaker-facter/filemaker-facts"; facter -d filemaker_errors
 #
 # HISTORY
 #   2015-02-07 simon_b: created file
 #   2015-03-27 simon_b: now only returning error messages
 #   2015-04-02 simon_b: Windows version
+#   2016-01-05 simon_b: now using constants from filemaker_util file
+#   2016-01-05 simon_b: now constrain results by time range
+
 
 require 'etc'
+require 'time'
 require "facter/filemaker/filemaker_utils"
 
 
 Facter.add('filemaker_errors') do
 
-  setcode do
-     # Change these if too few/too many errors getting reported.
-     events_to_check = 300
-     max_errors = 10
-     max_seconds = 2*60*60 - 1  # 2 hours, minus a second
-     oldest_error = Time.at (Time.now - max_seconds)
+     # From right now, how far back in time should we go?
+     oldest_error = Time.at (Time.now - MAX_SECONDS)
 
-     # Get recent FMS event data.
-     raw=tail(LOG_EVENTS,events_to_check)
-     error_lines = raw.scan(/.*\tError\t.*/)
+     # First read in a portion of the log data up to our maximum log lines.
+     raw_lines = tail(LOG_EVENTS,EVENTS_TO_CHECK)
+
+     # Further trim out by removing any non-error lines.
+     error_lines = raw_lines.scan(/.*\tError\t.*/)
+     period_lines = []
+     limited_lines = []
 
      if error_lines != []
-        # Going backwards would have some advantages, but I'll use a simpler approach here.
-        error_lines.each { |line|
-           if Time.parse (line[0..24]) >= oldest_error
+        # Loop over each line, adding those that match. 
+        # Going backwards could have some advantages. Also, chopping string
+        # from start of first line in match instead of looping over all.
+        error_lines.each do |line|
+           # Line's time stamp within range?
+           line_time = Time.parse (line[0..15])
+           is_period = line_time > oldest_error
+     
+           if is_period
+              period_lines << line
+           end
+        end
 
-        xxxxxxxxxxx
-
-        # Restrict to the last errors found.
-
-
-
-        max_errors = [max_errors,error_lines.count].min
-        error_lines_last = error_lines[-max_errors, max_errors]
-
-
-
-        # This could've been returned as a structured result, but it is a bit more readable as string.
-        error_lines_last.join("\n")
+        max_errors = [MAX_ERRORS,period_lines.count].min
+        # Starting from the end, get the last 'max_errors' lines.
+        limited_lines = period_lines[-max_errors, max_errors]        
      end
-  end
-end
 
+  setcode do
+     # This could've been returned as a structured result, but it is a bit more readable as string.
+     limited_lines.join("\n")
+  end
+
+end

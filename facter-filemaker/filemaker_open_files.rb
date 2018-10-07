1b9bf1838b7a0844978c1f5dd5f7f631266d7594
Simon Brown <simon_b@beezwax.net>
2018-10-07 14:34:15 -0700

    first commit

---
+++ facter-filemaker/filemaker_open_files.rb
@@ -0,0 +1,36 @@
+# FACT: FileMaker Server running components
+#
+# PURPOSE: List the currently open files
+#
+# NOTES:
+#
+#   No Windows version at this time.
+#
+#   Testing looks like this:
+#     FACTERLIB="/Users/simon_b/filemaker-facter/facter-filemaker"; facter -d filemaker_open_files
+#
+# HISTORY
+#   2018-10-07 simon_b: created file
+
+require_relative 'filemaker/filemaker_utils'
+
+
+Facter.add('filemaker_open_files') do
+
+  # Mac OS Version
+  confine :kernel => :darwin
+
+  setcode do
+
+     
+     # lsof: list everything beloning to fmserverd process but only print file paths.
+     # cut: strip the "n"
+     # grep: only list FileMaker files
+     # sort: sort path lines
+
+     raw = `/usr/sbin/lsof -Fn -p \`/usr/bin/pgrep fmserverd\` | /usr/bin/cut -c2-| /usr/bin/grep ".fmp12$" | /usr/bin/sort`
+     dataRows = raw.lines.to_a[1..-1].join
+
+     dataRows
+  end
+end
